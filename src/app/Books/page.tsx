6511bb47b2299a2db375cbfb588b3728825dc834
mehedi852-ccpc <mehedihasan958327@gmai.com>
2026-09-22 21:14:36 +0600

    com-project

--- src/app/Books/page.tsx
+++ src/app/Books/page.tsx
@@ -4,7 +4,7 @@ import iBook from "@/app/type";
 import BookCard from "@/component/BookCard";
 
 const Booksdata = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}booksData.json`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/booksData.json`);
 
   if (!res.ok) {
     throw new Error("Failed to fetch books data");
